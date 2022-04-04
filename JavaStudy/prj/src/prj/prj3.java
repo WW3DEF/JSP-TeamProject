30c04c808945730fce6c0dd25286fa09a62fa7ef
InDEF <zzangka1122@gmail.com>
2022-04-04 14:14:22 +0900

    자바3

---
+++ JavaStudy/prj/src/prj/prj3.java
@@ -0,0 +1,15 @@
+package prj;
+
+import java.util.Scanner;
+
+public class prj3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		System.out.println(A);
+	}
+
+}
