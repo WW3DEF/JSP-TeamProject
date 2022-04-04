a1abf96ad8d1375a6272eff341acad6c5788aad0
InDEF <zzangka1122@gmail.com>
2022-04-04 14:13:50 +0900

    자바2

---
+++ JavaStudy/prj/src/prj/prj2.java
@@ -0,0 +1,15 @@
+package prj;
+
+public class prj2 {
+	static int A = 0;		// 정수
+	static int B = 1;		// 정수
+	static double C = 3.14;		// 소수	float
+	static String D = "world";	// 문자열
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(A+B);
+		System.out.println((int)C);
+		System.out.println(D);
+	}
+
+}
