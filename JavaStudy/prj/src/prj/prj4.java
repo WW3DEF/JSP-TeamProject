66511323db03317e58977fa97acb2d90b533c83e
InDEF <zzangka1122@gmail.com>
2022-04-04 14:14:49 +0900

    자바4

---
+++ JavaStudy/prj/src/prj/prj4.java
@@ -0,0 +1,22 @@
+package prj;
+
+import java.util.Scanner;
+
+public class prj4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 for(초기화문; 조건문; 증감연산자) {
+		 	조건안에 실행할 것들
+		 }
+		 */
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		for (int i = 1; i < A; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
