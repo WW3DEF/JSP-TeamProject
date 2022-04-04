c34f8974c4bc4d2d667221e04ce216f4f15ef001
InDEF <zzangka1122@gmail.com>
2022-04-04 14:12:24 +0900

    자바1

---
+++ JavaStudy/prj/src/prj/prj1.java
@@ -0,0 +1,12 @@
+package prj;
+
+public class prj1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello");
+		System.out.println("HI");
+		System.out.println("Hello World!!");
+	}
+
+}
